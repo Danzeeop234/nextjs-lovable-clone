df06f6574c4412c9ef44e691f98c626c62feaf00
renatomarf76 <renatomarf76@egmail.com>
2025-07-01 15:59:43 -0300

    06-e2b-sandboxes

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,13 +1,21 @@
+import { Sandbox } from "@e2b/code-interpreter";
 import { createAgent, openai } from "@inngest/agent-kit";
+
 import { inngest } from "./client";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event, step }) => {
     if (!event.data?.value) {
       throw new Error("Missing 'value' in event data.");
     }
+    
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("vibe-nextjs-test11");
+      return sandbox.sandboxId;
+    });
 
     const codeAgent = createAgent({
       name: "code-agent",
@@ -23,8 +31,16 @@ export const helloWorld = inngest.createFunction(
       `Write the following snippet: ${event.data.value}`
     );
 
-    return { output };
-  }
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    });
+
+
+    
+    return { output, sandboxUrl };
+  },
 );
 
 
